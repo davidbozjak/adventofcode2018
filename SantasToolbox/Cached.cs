a6886d0a9bbdb6a781e3be234ae27e07622704c4
David Božjak <bozjak@gmail.com>
2018-12-02 07:39:45 +0100

    Cleaned up inputProvider by using Cached

---
+++ SantasToolbox/Cached.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SantasToolbox
+{
+    public class Cached<T> : IDisposable
+    {
+        private readonly Func<T> initializer;
+        private Lazy<T> value;
+
+        public Cached(Func<T> initializer)
+        {
+            this.initializer = initializer;
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.value = new Lazy<T>(this.initializer);
+        }
+
+        public void Dispose()
+        {
+            if (this.value.IsValueCreated && this.value.Value is IDisposable disposable)
+            {
+                disposable.Dispose();
+                this.Reset();
+            }
+        }
+
+        public T Value => this.value.Value;
+
+        public bool IsValueCreated => this.value.IsValueCreated;
+    }
+}
--- SantasToolbox/InputProvider.cs
+++ SantasToolbox/InputProvider.cs
@@ -7,17 +7,15 @@ namespace SantasToolbox
     public class InputProvider<T> : IEnumerator<T>
     {
         public delegate bool StringToTConverter(string input, out T result);
-
-        private readonly string filePath;
-        private StreamReader fileStream;
-        private StringToTConverter converter;
-
+        
+        private readonly StringToTConverter converter;
+        private readonly Cached<StreamReader> fileStream;
+        
         public InputProvider(string filePath, StringToTConverter converter)
         {
-            this.filePath = filePath;
             this.converter = converter;
 
-            this.fileStream = new StreamReader(this.filePath);
+            this.fileStream = new Cached<StreamReader>(() => new StreamReader(filePath));
         }
 
         public T Current { get; private set; }
@@ -31,7 +29,7 @@ namespace SantasToolbox
 
         public bool MoveNext()
         {
-            var line = this.fileStream.ReadLine();
+            var line = this.fileStream.Value.ReadLine();
 
             if (line?.Length <= 0)
             {
@@ -49,7 +47,7 @@ namespace SantasToolbox
 
         public void Reset()
         {
-            this.fileStream = new StreamReader(filePath);
+            this.fileStream.Reset();
         }
     }
 }

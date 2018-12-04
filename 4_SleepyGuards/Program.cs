7ad9cea6e06d91c643905562ea2c50ccbad91026
David Božjak <bozjak@gmail.com>
2018-12-04 07:26:57 +0100

    Init day 4

---
+++ 4_SleepyGuards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_SleepyGuards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

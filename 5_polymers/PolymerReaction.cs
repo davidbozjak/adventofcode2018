65d090ccafd8bb725ede1b6de6889f50076d7fcd
David Božjak <bozjak@gmail.com>
2018-12-05 06:45:04 +0100

    Day 5 init, solution for part 1

---
+++ 5_polymers/PolymerReaction.cs
@@ -0,0 +1,51 @@
+﻿using SantasToolbox;
+using System;
+using System.Text;
+
+namespace _5_polymers
+{
+    class PolymerReaction
+    {
+        static void Main(string[] args)
+        {
+            var polymerProvider = new InputProvider<string>("Input.txt", GetString);
+            polymerProvider.MoveNext();
+
+            string polymer = polymerProvider.Current;
+
+            var polymerBuilder = new StringBuilder(polymer);
+
+            const int reactDiff = 'a' - 'A';
+
+            for (bool hasReacted = true; hasReacted; )
+            {
+                hasReacted = false;
+                Console.WriteLine($"New loop, polymer length: {polymerBuilder.Length}");
+
+                for (int i = 1; i < polymerBuilder.Length; i++)
+                {
+                    var diff = Math.Abs(polymerBuilder[i] - polymerBuilder[i - 1]);
+
+                    if (diff == reactDiff)
+                    {
+                        hasReacted = true;
+                        polymerBuilder.Remove(i - 1, 2);
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"No more reactions, final polymer length: {polymerBuilder.Length}");
+
+            Console.WriteLine("");
+            Console.WriteLine("Done, Press any key co close.");
+            Console.ReadKey();
+        }
+
+        private static bool GetString(string value, out string result)
+        {
+            result = value;
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+}

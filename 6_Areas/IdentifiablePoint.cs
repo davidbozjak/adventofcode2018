ad93405c2428d52142ec7683996ce2f53700ebc6
David Božjak <bozjak@gmail.com>
2018-12-06 08:56:31 +0100

    extracted class into separate file

---
+++ 6_Areas/IdentifiablePoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace _6_Areas
+{
+    class IdentifiablePoint
+    {
+        private Point point;
+        private static int idCounter = 0;
+
+        public IdentifiablePoint(Point point)
+        {
+            this.point = point;
+            this.Id = idCounter++;
+        }
+
+        public int Id { get; }
+
+        public int X => this.point.X;
+
+        public int Y => this.point.Y;
+    }
+}
--- 6_Areas/LargestArea.cs
+++ 6_Areas/LargestArea.cs
@@ -98,23 +98,7 @@ namespace _6_Areas
             return points;
         }
 
-        class IdentifiablePoint
-        {
-            private Point point;
-            private static int idCounter = 0;
-
-            public IdentifiablePoint(Point point)
-            {
-                this.point = point;
-                this.Id = idCounter++;
-            }
-
-            public int Id { get; }
-
-            public int X => this.point.X;
-
-            public int Y => this.point.Y;
-        }
+        
 
         private static bool GetPoint(string value, out IdentifiablePoint result)
         {

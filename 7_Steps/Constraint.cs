76f35f82fe37edb944ad2427ac6f4bcb8575a656
David Božjak <bozjak@gmail.com>
2018-12-08 08:10:11 +0100

    Extracted classes into separate files

---
+++ 7_Steps/Constraint.cs
@@ -0,0 +1,17 @@
+﻿namespace _7_Steps
+{
+    class Constraint
+    {
+        public Constraint(string instruction)
+        {
+            this.OriginalInstruction = instruction;
+            this.First = instruction[5];
+            this.After = instruction[36];
+        }
+
+        public char First { get; }
+        public char After { get; }
+        public string OriginalInstruction { get; }
+
+    }
+}
---
+++ 7_Steps/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _7_Steps
+{
+    class Node
+    {
+        private readonly List<Node> parents = new List<Node>();
+
+        public Node(char Id)
+        {
+            this.Id = Id;
+            this.Duration = 61 + (Id - 'A');
+            this.CompletedAt = int.MinValue;
+        }
+
+        public char Id { get; }
+
+        public int Duration { get; }
+
+        public int CompletedAt { get; private set; }
+
+        public IReadOnlyList<Node> Parents => this.parents.AsReadOnly();
+
+        public void AddParent(Node n)
+        {
+            if (!this.parents.Select(w => w.Id).Any(w => w == n.Id))
+            {
+                this.parents.Add(n);
+            }
+        }
+
+        public void Start(int now)
+        {
+            this.CompletedAt = now + Duration;
+        }
+    }
+}

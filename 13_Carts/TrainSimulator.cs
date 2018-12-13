d0ab44f07f4a891de27cc7e63542adb3aea05046
David Božjak <bozjak@gmail.com>
2018-12-13 07:19:42 +0100

    added main load logic

--- 13_Carts/TrainSimulator.cs
+++ 13_Carts/TrainSimulator.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using SantasToolbox;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
 namespace _13_Carts
 {
-    class Program
+    class TrainSimulator
     {
         static void Main(string[] args)
         {
@@ -16,41 +17,58 @@ namespace _13_Carts
 
         private static World GetInitialState()
         {
+            var rowProvider = new InputProvider<string>("Input.txt", GetString);
+
+            while (rowProvider.MoveNext())
+            {
+                var row = rowProvider.Current;
+                foreach (var c in row)
+                {
+
+                }
+            }
+
+            return new World();
+        }
+
+        private static bool GetString(string value, out string result)
+        {
+            result = value;
+            return !string.IsNullOrWhiteSpace(value);
+        }
+    }
+
+    class Track
+    {
+        public Point Position { get; }
+
+        public ICollection<Track> NextTrack { get; }
+    }
+
+    class Cart
+    {
+        public Track Position { get; }
+
+        public void Move()
+        {
+
+        }
+    }
+
+    class World
+    {
+        public World()
+        {
 
         }
 
-        class Track
+        public ICollection<Track> Tracks { get; }
+
+        public ICollection<Cart> Carts { get; }
+
+        public void MakeStep()
         {
-            public Point Position { get; }
 
-            public ICollection<Track> NextTrack { get; }
-        }
-
-        class Cart
-        {
-            public Track Position { get; }
-
-            public void Move()
-            {
-
-            }
-        }
-
-        class World
-        {
-            public World()
-            {
-
-            }
-
-            public ICollection<Track> Tracks { get; }
-
-            public ICollection<Cart> Carts { get; }
-
-            public void MakeStep()
-            {
-
-            }
         }
     }
 }

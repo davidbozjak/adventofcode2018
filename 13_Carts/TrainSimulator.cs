352abf42b0407339d8503a86103bc7df696505f0
David Božjak <bozjak@gmail.com>
2018-12-13 07:16:06 +0100

    Init day 13 (incomplete)

---
+++ 13_Carts/TrainSimulator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace _13_Carts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var initialState = GetInitialState();
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        private static World GetInitialState()
+        {
+
+        }
+
+        class Track
+        {
+            public Point Position { get; }
+
+            public ICollection<Track> NextTrack { get; }
+        }
+
+        class Cart
+        {
+            public Track Position { get; }
+
+            public void Move()
+            {
+
+            }
+        }
+
+        class World
+        {
+            public World()
+            {
+
+            }
+
+            public ICollection<Track> Tracks { get; }
+
+            public ICollection<Cart> Carts { get; }
+
+            public void MakeStep()
+            {
+
+            }
+        }
+    }
+}

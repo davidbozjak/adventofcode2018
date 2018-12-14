b5c61147dcc60666deec863517c7b8d574947197
David Božjak <bozjak@gmail.com>
2018-12-14 20:51:04 +0100

    day 14 init

---
+++ 14_Recipes/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _14_Recipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int input = 2018;
+            const int minRecepiesToGenerate = input + 10;
+
+            var recepies = new LinkedList<int>();
+            var firstElf = recepies.AddLast(3);
+            var secondElf = recepies.AddLast(7);
+
+            var result = new StringBuilder(10);
+
+            while (recepies.Count < minRecepiesToGenerate)
+            {
+                var newRecipeSum = firstElf.Value + secondElf.Value;
+
+                foreach (var newRecipe in GetDigitsOfNum(newRecipeSum).Reverse())
+                {
+                    recepies.AddLast(newRecipe);
+
+                    if (recepies.Count > input && recepies.Count <= minRecepiesToGenerate)
+                    {
+                        result.Append(newRecipe.ToString());
+                    }
+                }
+
+                firstElf = GetNextRecipe(firstElf);
+                secondElf = GetNextRecipe(secondElf);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine($"Part1: {result}");
+            Console.ReadKey();
+
+            LinkedListNode<int> GetNextRecipe(LinkedListNode<int> recipe)
+            {
+                for (int i = 0, count = 1 + recipe.Value; i < count; recipe = recipe.Next ?? recepies.First, i++) ;
+
+                return recipe;
+            }
+        }
+
+        static IEnumerable<int> GetDigitsOfNum(int num)
+        {
+            for (int digit = num % 10; num > 0; num = num / 10, digit = num % 10)
+            {
+                yield return digit;
+            }
+        }
+    }
+}

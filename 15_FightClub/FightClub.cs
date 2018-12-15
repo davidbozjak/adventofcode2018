78bf1810768d2408e679944f26f066a46fac721e
David Božjak <bozjak@gmail.com>
2018-12-15 10:29:32 +0100

    Day 15 init

---
+++ 15_FightClub/FightClub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _15_FightClub
+{
+    class FightClub
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

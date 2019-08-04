77caec1c5c50f6adb0aff6e261c39d5ca9b75a66
David Božjak <bozjak@gmail.com>
2019-08-04 18:37:57 +0200

    Added spatial helper methods for Point

--- SantasToolbox/PointExtensions.cs
+++ SantasToolbox/PointExtensions.cs
@@ -14,5 +14,39 @@ namespace SantasToolbox
         {
             return point.Y * 1000 + point.X;
         }
+
+        public static Point Left(this Point point)
+        {
+            return new Point(point.X - 1, point.Y);
+        }
+
+        public static Point Right(this Point point)
+        {
+            return new Point(point.X - 1, point.Y);
+        }
+
+        public static Point Up(this Point point)
+        {
+            return new Point(point.X, point.Y - 1);
+        }
+
+        public static Point Down(this Point point)
+        {
+            return new Point(point.X, point.Y + 1);
+        }
+
+        public static bool IsNeighbour(this Point point, Point otherPoint)
+        {
+            if (point.X == otherPoint.X)
+            {
+                return Math.Abs(point.Y - otherPoint.Y) == 1;
+            }
+            else if (point.Y == otherPoint.Y)
+            {
+                return Math.Abs(point.X - otherPoint.X) == 1;
+            }
+
+            return false;
+        }
     }
 }
